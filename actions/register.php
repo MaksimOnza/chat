56e95014b9f993638dbd895fd052b1c34d05eaf7
MaksimOnza <Maximus83onza@gmail.com>
2022-06-09 18:12:52 +0300

    Update register.php

--- actions/register.php
+++ actions/register.php
@@ -1,5 +1,6 @@
 <?php
 namespace Chat;
+
 use function Chat\db\query;
 
 $login = $_REQUEST['login'] ?? '';
@@ -10,16 +11,22 @@ function valid_in_data($login, $last_activity, $errors = [])
 {
     if (empty($login)) {
         $errors[] = ("Извините, Вы ввели не полные данные.");
+        
         return false;
     }
-    if(!is_integer($last_activity))
+    
+    if(!is_integer($last_activity)) {
         return false;
+    }
+    
     return true;
 }
 
 if (valid_in_data($login, $last_activity, $errors)) {
-    query("INSERT INTO users(name, last_activity) VALUES(?,?)",
-        [1 => $login, 2 => $last_activity]);
+    query(
+        "INSERT INTO users(name, last_activity) VALUES(?,?)",
+        [1 => $login, 2 => $last_activity]
+    );
     header('Location: /index.php?path=login');
 } else {
     return render($_REQUEST['path'], ['errors' => $errors]);
