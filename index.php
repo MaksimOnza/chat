0dd046df5eba37074d22df872914f1faf4fdb6ad
MaksimOnza <Maximus83onza@gmail.com>
2022-06-09 18:15:08 +0300

    small refactoring

--- index.php
+++ index.php
@@ -10,9 +10,6 @@ session_start();
 require_once("sqlite/db.php");
 require_once("constants/consts.php");
 
-//ini_set('error_reporting', E_ALL);
-//ini_set('display_errors', true);
-
 $errors = [];
 $path = $_REQUEST['path'];
 $list_of_path = array(
@@ -34,6 +31,7 @@ $list_of_exception = array(
 function run_action($path, $list_of_path): string
 {
     $action = in_array($path, $list_of_path) ? 'actions/' . $path . '.php' : 'actions/error_404.php';
+    
     return include $action;
 }
 
@@ -41,9 +39,12 @@ function render($path, array $vars = []): string
 {
     extract($vars);
     ob_start();
+    
     require_once 'views/' . $path . '.php';
+    
     $content = ob_get_contents();
     ob_clean();
+    
     return $content;
 }
 
@@ -51,6 +52,7 @@ function render($path, array $vars = []): string
 if (!in_array($path, $list_of_exception)) {
     $content = run_action($path, $list_of_path);
     $template = render('base_template', ['content' => $content]);
+    
     print $template;
 } else {
     run_action($path, $list_of_path);
