988d1d63ff75f1e9e9b316223f3436d0f278f9b1
MaksimOnza <Maximus83onza@gmail.com>
2022-06-09 18:00:31 +0300

    syntax refactoring

--- actions/chat.php
+++ actions/chat.php
@@ -10,11 +10,14 @@ $messages = [];
 if (session_status() != SESSION_ACTIVE) {
     header('Location: /index.php?path=login');
 }
+
 $result = query_select('SELECT name FROM users');
 $users = array();
+
 foreach ($result as $user) {
     $users[] = $user['name'];
 }
 
 $content = array('login' => $login, 'users' => $users, 'messages' => $messages, 'emojy' => EMOJY_ARRAY);
+
 return render('chat', $content);
