67e1cda457029c177e437d9696eeb9224756ba62
MaksimOnza <Maximus83onza@gmail.com>
2022-06-09 18:02:56 +0300

    syntax refactoring

--- actions/login.php
+++ actions/login.php
@@ -17,9 +17,11 @@ $user = query_select("SELECT * FROM users WHERE name = ?", [1 => $login])[0];
 $_SESSION['user_id'] = $user['id'];
 $_SESSION['user_name'] = $user['name'];
 $_SESSION['session_id'] = session_id();
+
 if (empty($user)) {
     $_SESSION['user_name'] = $guest;
     $_SESSION['user_id'] = 0;
     $_SESSION['session_id'] = session_id();
 }
+
 header('Location: /index.php?path=chat');
